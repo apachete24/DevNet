c815bb087290972a0500a9cf66802eba8803db08
dasonodas <49148040+dasonodas@users.noreply.github.com>
2020-05-04 23:25:15 +0200

    Update Conexion_ssh.py

--- Conexion_ssh.py
+++ Conexion_ssh.py
@@ -2,15 +2,14 @@ from netmiko import ConnectHandler
 
 ssh_client = ConnectHandler(
 
-    device__type = 'cisco_ios',
-    host = '192.168.0.18',
+    device_type = 'cisco_ios',
+    host = '192.168.0.19',
     port = 22,
     username = 'cisco',
-    password = 'cisco123',
+    password = 'cisco123!',
 
 )
 # enviaar ejecutar unos comando simples y mostrar lo que devuelve
 
 output = ssh_client.send_command("show ip int brief")
 print("IP interface status and configuration:\n{}\n".format(output))
-
